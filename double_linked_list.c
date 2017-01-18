95eea2d3fbaeec274cbc567348354d4589d320fc
Mikhail <miksolodovnikov@gmail.com>
2017-01-18 19:11:07 +0500

    Add possibility to calculate list length and compare lists. Fix minor bugs.

--- double_linked_list.c
+++ double_linked_list.c
@@ -10,7 +10,7 @@ struct Node {
     struct Node* prev;
 };
 
-struct Node* createNode(int data) {
+struct Node* create_node(int data) {
     struct Node* node = (struct Node*)malloc(sizeof(struct Node));
     node->data = data;
     node->next = NULL;
@@ -18,23 +18,19 @@ struct Node* createNode(int data) {
     return node;
 }
 
-struct Node* initHeadNode(int data) {
-    return createNode(data);
-}
-
-void addFirst(struct Node* head, int data) {
-    struct Node* node = createNode(data);
-    if (head == NULL) {
-        head = node;
-        return;
+struct Node* add_first(struct Node* head, int data) {
+    struct Node* node = create_node(data);
+    if(head == NULL) {
+        return node;
     }
     head->prev = node;
     node->next = head;
     head = node;
+    return head;
 }
 
-void addLast(struct Node* head, int data) {
-    struct Node* node = createNode(data);
+void add_last(struct Node* head, int data) {
+    struct Node* node = create_node(data);
     if (head == NULL) {
         head = node;
         return;
@@ -45,7 +41,7 @@ void addLast(struct Node* head, int data) {
     node->prev = current;
 }
 
-void printList(struct Node* head) {
+void print_list(struct Node* head) {
     printf("\n[ ");
     struct Node* node = head;
     while (node != NULL) {
@@ -68,11 +64,14 @@ void clear(struct Node* head) {
     head = NULL;
 }
 
-void clearNode(struct Node* head, struct Node* node) {
+void clear_node(struct Node* head, struct Node* node) {
     if (head == node) {
-        struct Node* tmp = head;
-        head = head->next;
-        free(head);
+        if (head->next != NULL) {
+            head->data = head->next->data;
+            head->next = head->next->next;
+        } else {
+            head = NULL;
+        }
     } else {
         node->prev->next = node->next;
         if (node->next != NULL) node->next->prev = node->prev;
@@ -80,40 +79,80 @@ void clearNode(struct Node* head, struct Node* node) {
     }
 }
 
+int length(struct Node* head) {
+    if (head == NULL) return 0;
+    int len = 1;
+    struct Node* current = head;
+    while (current->next != NULL) {
+        current = current->next;
+        len++;
+    }
+    return len;
+}
+
+int compare_lists(struct Node* head1, struct Node* head2) {
+    if (length(head1) != length(head2) || head1->data != head2->data) return 0;
+    struct Node* tmp1 = head1;
+    struct Node* tmp2 = head2;
+    while (tmp1->next != NULL && tmp2->next != NULL) {
+        if (tmp1->data != tmp2->data) {
+            return 0;
+        }
+        tmp1 = tmp1->next;
+        tmp2 = tmp2->next;
+    }
+    return 1;
+}
 
 // Tests for linked list API.
-/* int main() */
-/* { */
+/* int main() { */
 /*     struct Node* h0 = NULL; */
+/*     printf("0 == %d\n", length(h0)); */
 /*     clear(h0); */
-/*     struct Node* h1 = initHeadNode(0); */
-/*     printList(h1); */
+/*     struct Node* h1 = create_node(0); */
+/*     printf("1 == %d\n", length(h1)); */
+/*     print_list(h1); */
 /*     clear(h1); */
 
-/*     struct Node* h2 = initHeadNode(5); */
-/*     printList(h2); */
-/*     clearNode(h2, h2); */
+/*     struct Node* h2 = create_node(4); */
+/*     print_list(h2); */
+/*     clear_node(h2, h2); */
 
-/*     struct Node* h3 = initHeadNode(5); */
-/*     printList(h3); */
-/*     addLast(h3, 6); */
-/*     printList(h3); */
+/*     struct Node* h3 = create_node(5); */
+/*     print_list(h3); */
+/*     add_last(h3, 6); */
+/*     print_list(h3); */
 /*     struct Node* n = h3->next; */
-/*     clearNode(h3, n); */
-/*     printList(h3); */
+/*     clear_node(h3, n); */
+/*     print_list(h3); */
 /*     clear(h3); */
 
-/*     struct Node* h4 = initHeadNode(0); */
+/*     struct Node* h4 = create_node(0); */
+/*     struct Node* h5 = create_node(0); */
 
 /*     for (int i=1; i<10; ++i) { */
 /*         if (i % 2 == 0) { */
-/*             addFirst(h4, i); */
+/*             h4 = add_first(h4, i); */
+/*             h5 = add_first(h5, i); */
 /*         } else { */
-/*             addLast(h4, i); */
+/*             add_last(h4, i); */
+/*             add_last(h5, i); */
 /*         } */
 /*     } */
-/*     printList(h4); */
+/*     printf("10 == %d\n", length(h4)); */
+/*     print_list(h4); */
+/*     print_list(h5); */
+/*     printf("true == %d\n", compare_lists(h4, h5)); */
+/*     clear_node(h5, h5); */
+/*     print_list(h4); */
+/*     print_list(h5); */
+/*     printf("false == %d\n", compare_lists(h4, h5)); */
+/*     clear_node(h5, h5->next); */
+/*     print_list(h4); */
+/*     print_list(h5); */
+/*     printf("false == %d\n", compare_lists(h4, h5)); */
 /*     clear(h4); */
+/*     clear(h5); */
 
 /*     return 0; */
 /* } */
--- double_linked_list.h
+++ double_linked_list.h
@@ -3,10 +3,11 @@
  *Necessary to initialise head node before start working with list.
  */ 
 
-struct Node* createNode(int data);
-struct Node* initHeadNode(int data);
-void addFirst(struct Node* head, int data);
-void addLast(struct Node* head, int data);
-void printList(struct Node* head);
+struct Node* create_node(int data);
+struct Node* add_first(struct Node* head, int data);
+void add_last(struct Node* head, int data);
+void print_list(struct Node* head);
 void clear(struct Node* head);
-void clearNode(struct Node* head, struct Node* node);
+void clear_node(struct Node* head, struct Node* node);
+int compare_lists(struct Node* head1, struct Node* head2);
+int length(struct Node* head);
